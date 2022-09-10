e560dcaa33092d3acc7cf21b338c386d269ffe7c
Arnaud <arnaudly.pro@gmail.com>
2022-09-10 08:38:20 +0200

    Modif parsing and dist calc (all atoms)

--- main.py
+++ main.py
@@ -1,48 +1,54 @@
 import numpy as np
 import pandas as pd
-from Bio.PDB.PDBParser import PDBParser
 
-file = "1gcn.pdb"
-file_name = "Gluc"
 DOPE = pd.read_csv('dope.par.txt', ' ', header=None)
 
 #Create PDBParser object
-parser = PDBParser() 
-prot = parser.get_structure(file_name, file)
+with open("1gcn.pdb", 'r') as f:
+    atlist = {}
+    atnum = []
+    atname = []
+    atcoordx = []
+    atcoordy = []
+    atcoordz = []
+    atcoord = [] 
+    for line in f:
+        if line.startswith('ATOM'):
+            if line[13:17].strip() == 'CA':
+                atnum.append(line[6:12].strip())
+                atname.append(line[17:20].strip())
+                atcoordx.append(float(line[30:38]))
+                atcoordy.append(float(line[38:46]))
+                atcoordz.append(float(line[46:54]))
 
-#Obtain residues' name and CA atoms' coordinates
-for model in prot:
-   for chain in model:
-        ca_coord = []
-        ca_name = []
-        for residue in chain:
-            if residue.has_id('CA'):
-                ca_coord.append(residue['CA'].get_coord())
-                ca_name.append(residue.get_resname())
 
-#Calculate distance between CA atoms i and i+2
+for i in range(len(atnum)):
+    atcoord.append(np.array([atcoordx[i], atcoordy[i], atcoordz[i]]))
+    atlist[atnum[i] + atname[i]] = [atcoord[i]]
+
+
+#Calculate distance between CA atoms i and all atoms, starting from i+3
 ca_dist = []
 ca_dist_asso = {}
-for i in range(len(ca_coord)):
-    if i >= (len(ca_coord)-2):
-        break
-    else: 
-        diff_coord = ca_coord[i] - ca_coord[i+2]
-        ca_dist.append(np.sqrt(np.sum(diff_coord * diff_coord)))
-
-#Associate pair of CA with their distance
-for i in range(len(ca_dist)):
-    key_name = str(i+1) + ca_name[i] + '_' + str(i+3) + ca_name[i + 2]
-    ca_dist_asso[key_name] = ca_dist[i]
+for i in range(len(atlist)-2):
+        for n in range(3, len(atlist)-i):
+            diff_coord = atcoord[i] - atcoord[i+n]
+            ca_dist.append(np.sqrt(np.sum(diff_coord * diff_coord)))
 
 
-ca_dope_asso = {}
-value_ang = np.arange(0, 15.5, 0.5)
-value_dope = DOPE.iloc[:, 4:]
-value_asso = {}
-for i in range(len(DOPE)):
-    for n in range(30):
-        value_asso[value_ang[n]] = 
+# #Associate pair of CA with their distance
+# for i in range(len(ca_dist)):
+#     key_name = str(i+1) + ca_name[i] + '_' + str(i+3) + ca_name[i + 2]
+#     ca_dist_asso[key_name] = ca_dist[i]
+
+
+# ca_dope_asso = {}
+# value_ang = np.arange(0, 15.5, 0.5)
+# value_dope = DOPE.iloc[:, 4:]
+# value_asso = {}
+# for i in range(len(DOPE)):
+#     for n in range(30):
+#         value_asso[value_ang[n]] = 
 
 
 # for i in range(len(DOPE)):
