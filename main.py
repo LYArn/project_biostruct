9337540d70fa8ac8ab21193ba2f1c639f9ff0052
Arnaud <arnaudly.pro@gmail.com>
2022-09-13 14:04:17 +0200

    Defining DOPE Dataframe as a function

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ class Structure:
             self.atcoord.append(np.array([x[i], y[i], z[i]]))
 
 class Residue(Structure):
+    value_ang = np.arange(0.5, 15.5, 0.5)
     value_dope = {}
 
     def __init__(self):
@@ -32,7 +33,16 @@ class Residue(Structure):
         self.ca_name = []
         self.result = {}
 
-
+    def init_dope(self, DOPE):
+        #Create dataframe of DOPE value with Angstrom distance as column's name
+        col = ['res1', 'res2']
+        for i in range(len(self.value_ang)):
+            col.append(self.value_ang[i])
+        self.value_dope = DOPE[(DOPE[1]=='CA') & (DOPE[3]=='CA')]
+        self.value_dope = self.value_dope.drop([1, 3], axis = 1)
+        self.value_dope = self.value_dope.reset_index(drop=True)
+        self.value_dope = self.value_dope.set_axis(col, axis = 1)
+    
     def distance(self):
         """ Calculate distance between CA residue i and other residues i, with i > 3
             Return list of distances between CA and list of CA pairs (names with residues' number)"""
@@ -42,25 +52,16 @@ class Residue(Structure):
                 self.ca_dist.append(np.sqrt(np.sum(diff_coord * diff_coord)))
                 self.ca_name.append(self.atname[i] + '_' + self.atname[i+n])
 
-    def cal_pdf(self,DOPE):
-        self.distance()
-        value_ang = np.arange(0.5, 15.5, 0.5)
+    def potential_score(self):
         self.asso_name_index = {}
         self.asso_name_dist = {}
         self.asso_name_ang = {}
 
-        #Create dataframe of DOPE value with Angstrom distance as column's name
-        col = ['res1', 'res2']
-        for i in range(len(value_ang)):
-            col.append(value_ang[i])
-        value_dope = DOPE[(DOPE[1]=='CA') & (DOPE[3]=='CA')]
-        value_dope = value_dope.drop([1, 3], axis = 1)
-        value_dope = value_dope.reset_index(drop=True)
-        value_dope = value_dope.set_axis(col, axis = 1)
-
+        self.distance()
+        self.init_dope(DOPE)
         #Select row of interest in value_dope for each experimental distance
         for i in range(len(self.ca_name)):
-            for index, row in value_dope.iterrows():
+            for index, row in self.value_dope.iterrows():
                 if self.ca_name[i] == f"{row['res1']}_{row['res2']}":
                     self.asso_name_index[f"{i+1}{self.ca_name[i]}"] = index
         
@@ -70,25 +71,28 @@ class Residue(Structure):
         
         #Select correct distance in Angstrom for each experimental distance 
         for n in self.asso_name_index.keys():
-            for i in range(len(value_ang)):
-                if self.asso_name_dist[n] > value_ang[-1]:
-                    self.asso_name_ang[n] = value_ang[-1]
+            for i in range(len(self.value_ang)):
+                if self.asso_name_dist[n] > self.value_ang[-1]:
+                    self.asso_name_ang[n] = self.value_ang[-1]
                     break
-                elif self.asso_name_dist[n] > value_ang[i]:
+                elif self.asso_name_dist[n] > self.value_ang[i]:
                     pass
-                elif self.asso_name_dist[n] < value_ang[i]:
-                    self.asso_name_ang[n] = value_ang[i]
+                elif self.asso_name_dist[n] < self.value_ang[i]:
+                    self.asso_name_ang[n] = self.value_ang[i]
                     break
                 
         #Values of index and col being determined, associating pdf with CA pairs
         for i in self.asso_name_index.keys():
-            self.result[i] = value_dope.loc[value_dope.index[self.asso_name_index[i]], self.asso_name_ang[i]]
+            self.result[i] = self.value_dope.loc[self.value_dope.index[self.asso_name_index[i]], self.asso_name_ang[i]]
+
 
 if __name__ == "__main__":
     DOPE = pd.read_csv('dope.par.txt', ' ', header=None)
+
     with open("1gcn.pdb", 'r') as file:
-        gluc = Residue() #Create instance of class Residues
-        gluc.pars(file) #Parse glucagon .pdb file
-        gluc.cal_pdf(DOPE) #Associate pdf value with CA pair distances
-        gluc_total = sum(gluc.result.values())
-        print(f'Total energy score is {gluc_total}')
+        prot = Residue() #Create instance of class Residues
+        prot.pars(file) #Parse glucagon .pdb file
+    
+    prot.potential_score() #Associate pdf value with CA pair distances
+    prot_total = sum(prot.result.values())
+    print(f'Total energy score is {prot_total}')
